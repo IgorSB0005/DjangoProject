90164fed1934429d46904de79881ba0798770f36
IgorSB0005 <igor.shpakov05@gmail.com>
2024-12-25 17:29:19 +0100

    Final project element
    add users,post,sign in, sign up, logout and create new posts

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default="nonImage.png",blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None),
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Post
+from . import forms
+from django.http import HttpResponse
 
 def posts_list(request):
-    return render(request, 'posts/post_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/post_list.html', {'posts' : posts})
+
+
+def post_page(requests, slug):
+    posts = Post.objects.get(slug=slug)
+    return render(requests, 'posts/post_page.html', {'posts': posts})
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    if request.method == 'POST':
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            newPost = form.save(commit=False)
+            newPost.author = request.user
+            newPost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', {'form' : form})
